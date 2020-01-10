17cd94a4ed4470ccf7b9c7c48d17901b64b2f953
idawud <ismaildawud96@gmail.com>
2020-01-10 10:05:23 +0000

    initial :: raw project

---
+++ src/main/java/io/turntabl/DeveloperProfile/DeveloperProfileApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.DeveloperProfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/DeveloperProfile/DeveloperProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.DeveloperProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
